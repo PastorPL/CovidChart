45bb266a47e64956f600ea229b50f0f137b32065
Michal <michal.wieckowskii@gmail.com>
2020-03-29 20:36:49 +0200

    Deploy to Heroku

--- src/main/java/com/pixelfreak/service/GitHubService.java
+++ src/main/java/com/pixelfreak/service/GitHubService.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -63,37 +62,84 @@ public class GitHubService {
                     BufferedReader in = new BufferedReader(streamReader);
                     Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
                     for (CSVRecord record : records) {
-                        final Map<String, String> valueMap = record.toMap();
-                        final Entry entry = new Entry();
-                        entry.setProvince(valueMap.get("Province/State"));
-                        entry.setCountry(valueMap.get("Country/Region"));
-                        entry.setConfirmed(Integer.parseInt(valueMap.get("Confirmed")));
-                        entry.setDeaths(Integer.parseInt(valueMap.get("Deaths")));
-                        entry.setRecovered(Integer.parseInt(valueMap.get("Recovered")));
-                        entry.setLat(Double.parseDouble(valueMap.get("Latitude")));
-                        entry.setLon(Double.parseDouble(valueMap.get("Longitude")));
-
-                        String lastUpdateString = valueMap.get("Last Update");
-                        if (lastUpdateString.contains("T")) {
-                            lastUpdateString = lastUpdateString.split("T")[0];
-                            final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-d");
-                            final LocalDate localDate = LocalDate.parse(lastUpdateString, localDateFormat);
-                            entry.setLastUpdate(localDate);
+                        if(date.isEqual(LocalDate.of(2020, 3, 22))){
+                            this.processCVSFileFrom2203(entryList, record);
+                        } else if(date.isBefore(LocalDate.of(2020, 3, 22))) {
+                            this.processCVSFileBefor2203(entryList, record);
                         } else {
-                            lastUpdateString = lastUpdateString.split(" ")[0];
-                            final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-                            final LocalDate localDate = LocalDate.parse(lastUpdateString, localDateFormat);
-                            entry.setLastUpdate(localDate);
+                            this.processCVSFileAfter2203(entryList, record);
                         }
-                        entryList.add(entry);
+
                     }
                 }
             }
             return entryList;
         } catch (Exception e) {
+            e.printStackTrace();
             log.error(e.getLocalizedMessage());
             return new ArrayList<>();
         }
+    }
+
+    private void processCVSFileAfter2203(List<Entry> entryList, CSVRecord record) {
+        final Map<String, String> valueMap = record.toMap();
+        final Entry entry = new Entry();
+        entry.setProvince(valueMap.get("Province_State"));
+        entry.setCountry(valueMap.get("Country_Region"));
+        entry.setConfirmed(Integer.parseInt(valueMap.get("Confirmed")));
+        entry.setDeaths(Integer.parseInt(valueMap.get("Deaths")));
+        entry.setRecovered(Integer.parseInt(valueMap.get("Recovered")));
+        entry.setLat(Double.parseDouble(valueMap.get("Lat")));
+        entry.setLon(Double.parseDouble(valueMap.get("Long_")));
+        String lastUpdateString = valueMap.get("Last_Update");
+        lastUpdateString = lastUpdateString.split(" ")[0];
+        final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        final LocalDate localDate = LocalDate.parse(lastUpdateString, localDateFormat);
+        entry.setLastUpdate(localDate);
+        entryList.add(entry);
+    }
+
+    private void processCVSFileFrom2203(List<Entry> entryList, CSVRecord record) {
+        final Map<String, String> valueMap = record.toMap();
+        final Entry entry = new Entry();
+        entry.setProvince(valueMap.get("Province_State"));
+        entry.setCountry(valueMap.get("Country_Region"));
+        entry.setConfirmed(Integer.parseInt(valueMap.get("Confirmed")));
+        entry.setDeaths(Integer.parseInt(valueMap.get("Deaths")));
+        entry.setRecovered(Integer.parseInt(valueMap.get("Recovered")));
+        entry.setLat(Double.parseDouble(valueMap.get("Lat")));
+        entry.setLon(Double.parseDouble(valueMap.get("Long_")));
+        String lastUpdateString = valueMap.get("Last_Update");
+        lastUpdateString = lastUpdateString.split(" ")[0];
+        final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("MM/dd/yy");
+        final LocalDate localDate = LocalDate.parse(lastUpdateString, localDateFormat);
+        entry.setLastUpdate(localDate);
+        entryList.add(entry);
+    }
+
+    private void processCVSFileBefor2203(List<Entry> entryList, CSVRecord record) {
+        final Map<String, String> valueMap = record.toMap();
+        final Entry entry = new Entry();
+        entry.setProvince(valueMap.get("Province/State"));
+        entry.setCountry(valueMap.get("Country/Region"));
+        entry.setConfirmed(Integer.parseInt(valueMap.get("Confirmed")));
+        entry.setDeaths(Integer.parseInt(valueMap.get("Deaths")));
+        entry.setRecovered(Integer.parseInt(valueMap.get("Recovered")));
+        entry.setLat(Double.parseDouble(valueMap.get("Latitude")));
+        entry.setLon(Double.parseDouble(valueMap.get("Longitude")));
+        String lastUpdateString = valueMap.get("Last Update");
+        if (lastUpdateString.contains("T")) {
+            lastUpdateString = lastUpdateString.split("T")[0];
+            final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-d");
+            final LocalDate localDate = LocalDate.parse(lastUpdateString, localDateFormat);
+            entry.setLastUpdate(localDate);
+        } else {
+            lastUpdateString = lastUpdateString.split(" ")[0];
+            final DateTimeFormatter localDateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            final LocalDate localDate = LocalDate.parse(lastUpdateString, localDateFormat);
+            entry.setLastUpdate(localDate);
+        }
+        entryList.add(entry);
     }
 
 }
